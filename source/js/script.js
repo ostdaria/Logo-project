512bff7a1977eae30d1ed4f0eea1af9f32d005d7
Daria <dashaostapuk98@gmail.com>
2022-02-03 21:42:18 +0300

    добавление доп правок

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,50 @@ mainNavToggle.addEventListener("click", function() {
     mainNav.classList.remove("page-header__navigation--opened");
   }
 });
+
+
+var formElement = document.querySelector("form");
+var userName = document.querySelector(".form__input--name");
+var userPhone = document.querySelector(".form__input--phone");
+var userEmail = document.querySelector(".form__input--email");
+var userPacking = document.querySelector(".form__input--packing");
+var userAddress = document.querySelector(".form__input--address");
+var userComment = document.querySelector(".form__input--comment");
+var buttonSubmit = document.querySelector(".cart__button-link");
+
+function FormData (name, phone, email, packaging, address, comment) {
+  this.name = name;
+  this.phone = phone;
+  this.email = email;
+  this.packaging = packaging;
+  this.address = address;
+  this.comment = comment;
+
+  this.getUserData = function() {
+    alert(formData);
+    console.log();
+  };
+}
+
+// formElement.onsubmit = async (data) => {
+//   data.preventDefault();
+
+//   buttonSubmit.("onsubmit", function () {
+//     var formData = new FormData();
+//     formData.append("userForm", "#userForm");
+    
+//     alert(formData);
+//     console.log(formData);
+//   })
+// } 
+
+
+var formData = new FormData(userName, userPhone, userEmail, userPacking, userAddress, userComment);
+
+formData.append();
+
+formData.getUserData();
+
+// UserData.prototype.getUserData = function() {
+//   alert(formData);
+// };
